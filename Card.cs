fc1ebe18f4f5e674b107db852680226c764e153e
WillAshfield <127102015+WillAshfield@users.noreply.github.com>
2023-03-07 22:24:24 +0000

    Update 2 on Card

--- Card.cs
+++ Card.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +12,60 @@ namespace CMP1903M_A01_2223
         //Value: numbers 1 - 13
         //Suit: numbers 1 - 4
         //The 'set' methods for these properties could have some validation
-        public int Value { get; set; }
-        public int Suit { get; set; }
+        private int _value;
+        public int Value
+        {
+            get { return _value; }
+            set
+            {
+                if (value >= 1 && value <= 13)
+                {
+                    _value = value;
+                }
+                else
+                {
+                    throw new Exception("Value out of range");
+                };
+            }
+        }
+
+        private int _suit;
+        public int Suit
+        {
+            get { return _suit; }
+            set
+            {
+                if (value >= 1 && value <= 4)
+                {
+                    _suit = value;
+                }
+                else
+                {
+                    throw new Exception("Suit out of range");
+                };
+            }
+        }
+
+        public string Description()
+        {
+            var suitName = Suit switch
+            {
+                1 => "Heart",
+                2 => "Club",
+                3 => "Diamond",
+                _ => "Spade"
+            };
+
+            var cardName = Value switch
+            {
+                1 => "A",
+                10 => "J",
+                11 => "Q",
+                12 => "K",
+                _ => Value.ToString()
+            };
+
+            return cardName + suitName;
+        }
     }
 }
