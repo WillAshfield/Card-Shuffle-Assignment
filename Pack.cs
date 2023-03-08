501f2d094d95aba0853acacae6f7e6bd953fd4eb
WillAshfield <127102015+WillAshfield@users.noreply.github.com>
2023-03-08 17:25:37 +0000

    Update V3 Pack

--- Pack.cs
+++ Pack.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,10 +10,10 @@ namespace CMP1903M_A01_2223
     class Pack
     {
         public List<Card> deck;
-
         public Pack()
         {
             deck = new List<Card>();
+
 
             for (var suit = 1; suit <= 4; suit++)
             {
@@ -28,23 +29,69 @@ namespace CMP1903M_A01_2223
 
         }
 
-        public static bool shuffleCardPack(int typeOfShuffle)
+        public static bool shuffleCardPack(int typeOfShuffle, List<Card> deck)
         {
-            return true;
+
+            List<Card> emptyDeck = new List<Card>();
+
+            Random rnd = new Random();
+
             //Shuffles the pack based on the type of shuffle
+            // If the user picks the number 1, Fisher-Yates Shuffle
+            if (typeOfShuffle == 1)
+            {
+                for (int i = 0; i < 52; i++)
+                {
+                    int num = rnd.Next(deck.Count);
+                    emptyDeck.Add(deck[num]);
+                    deck.Remove(deck[num]);
+                }
+                for (int i = 0; i < emptyDeck.Count; i++)
+                {
+                    deck.Add(emptyDeck[i]);
+                }
+                return true;
+            }
+            // If the user picks the number 2, Riffle Shuffle
+            else if (typeOfShuffle == 2)
+            {
+                for (int i = 0; i < 25; i++)
+                {
+                    emptyDeck.Add(deck[i]);
+                    emptyDeck.Add(deck[i + 26]);
+                }
+                deck.Clear();
+                for (int i = 0; i < emptyDeck.Count; i++)
+                {
+                    deck.Add(emptyDeck[i]);
+                }
+                return true;
+            }
+            // If the user picks the number 3, the deck will not be shuffled and just return
+            else if (typeOfShuffle == 3)
+            {
+                return true;
+            }
 
+            return false;
         }
-        public static Card deal()
+        public static Card deal(List<Card> deck)
         {
-            return new Card();
+            Card dealt = deck[0];
+
             //Deals one card
-
+            return dealt;
         }
-        public static List<Card> dealCard(int amount)
+        public static List<Card> dealCards(int amount, List<Card> deck)
         {
-            return new List<Card>();
 
+            List<Card> dealtCards = new List<Card>();
             //Deals the number of cards specified by 'amount'
+            for (int i = 0; i < amount; i++)
+            {
+                dealtCards.Add(deck[i]);
+            }
+            return dealtCards;
         }
     }
 }
